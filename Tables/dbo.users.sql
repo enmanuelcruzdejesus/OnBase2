3d2b4460c587ce647a1f350612982fa4e911bcce
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2019-05-04 17:15:12 -0400

    adding users table

---
+++ Tables/dbo.users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[users]
+(
+[id] [int] NULL,
+[firstname] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
+[lastname] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
+[user] [varchar] (50) COLLATE Latin1_General_CI_AS NULL
+) ON [PRIMARY]
+GO
